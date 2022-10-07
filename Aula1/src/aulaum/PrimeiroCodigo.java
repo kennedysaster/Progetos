47ae1ddd9117e0f4ad6f672e28a415727ab0b8a6
Sigep <Sigep@192.168.1.44>
2022-10-06 23:07:11 -0300

    Declarando variaveis

--- Aula1/src/aulaum/PrimeiroCodigo.java
+++ Aula1/src/aulaum/PrimeiroCodigo.java
@@ -1,5 +1,20 @@
 package aulaum;
 
 public class PrimeiroCodigo {
- /*teste*/
+	static String valormaior = "--------------";
+ public static void main (String[] args) {
+	 
+	 int idade1 = 42;
+	 int idade2 = 39;
+	 String valor = "Marcos Kennedy";
+	 String valor2 = "Renata Kelly";
+	 
+	 if (idade1 > idade2) {
+	 
+	 System.out.println("Idade Maior"+ ":" + valor + idade1 + valormaior);
+ }
+	 else {
+		 System.out.println("Idade Menor : " + valor2 + idade2);
+	 }
+}
 }
